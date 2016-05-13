5dc984960fa69c82ea9379258223215072af3308
ks-s-a <s.ksenofontov@sup.com>
2016-05-13 21:37:22 +0300

    add comments and minor fixes

--- passport-example.js
+++ passport-example.js
@@ -1,3 +1,5 @@
+// User authtorization with PassportJS
+
 var app = require('express')();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
@@ -5,74 +7,85 @@ var session = require('cookie-session');
 var passport = require('passport');
 var LocalStratagy = require('passport-local');
 
+// Set middlewares
 app.use(cookieParser()); // req.cookies
 app.use(session({keys: ['key']})); // req.session
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Set authtorization stratagy
 passport.use(new LocalStratagy(function (username, pass, done) {
-  if (username !== 'admin' || pass !== 'admin') {
+  if (username !== 'admin' || pass !== 'admin')
     return done(null, false)
-  } else {
-    return done(null, {username: username});
-  }
+
+  done(null, {username: username});
 }));
 
+// Method for saving user in session
 passport.serializeUser(function (user, done) {
-  return done(null, user.username);
+  done(null, user.username);
 });
 
+// Method for restore user from session
 passport.deserializeUser(function (username, done) {
-  return done(null, {username: username});
+  done(null, {username: username});
 });
 
+// Main page for everyone
 app.get('/', function (req, res) {
-  return res
+  res
     .status(200)
     .send('Это главная страница!');
 });
 
+// Login form
 app.get('/login', function (req, res) {
-  return res
+  res
     .status(200)
-    .send('<form action="/login" method="post">' +
-      'Login: ' +
-      '<input type="text" name="username" />' +
-      '<input type="password" name="password" />' +
-      '<input type="submit" />' +
-    '</form>');
+    .send(
+      '<form action="/login" method="post">' +
+        'Login: ' +
+        '<input type="text" name="username" />' +
+        '<input type="password" name="password" />' +
+        '<input type="submit" />' +
+      '</form>');
 });
 
+// Login request handler
 app.post('/login', bodyParser.urlencoded({ extended: false }), passport.authenticate('local', {
   successRedirect: '/user',
   failureRedirect: '/login'
 }));
 
+// Page for user settings
 app.use('/user/settings', mustBeAuthentificated, function (req, res) {
   res
     .status(200)
     .send('Совершенно секретная часть. Только для тебя, ' + req.user.username);
 });
 
+// User cabinet
 app.use('/user', mustBeAuthentificated, function (req, res) {
   res
     .status(200)
     .send('Привет, ' + req.user.username);
 });
 
+// Logout page
 app.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('/');
+  req.logout(); // execute passportJS logout
+  res.redirect('/'); // go to main page
 });
+
+// Middleware for login checking
+function mustBeAuthentificated (req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+
+  res.redirect('/login'); // go to login page
+}
 
 app.listen(8000, function () {
   console.log('The application was launched on 8000 port!');
 });
 
-function mustBeAuthentificated (req, res, next) {
-  if ( req.isAuthenticated() ) {
-    next();
-  } else {
-    res.redirect('/login');
-  }
-}
--- session.js
+++ session.js
@@ -1,16 +1,20 @@
+// Cookies and user session
 var app = require('express')();
 var cookieParser = require('cookie-parser');
 var session = require('cookie-session');
 
+// Set middlewares
 app.use(cookieParser());
 app.use(session({
   keys: ['key'],
 }));
 
-app.get('/', function (req, res, next) {
-  var count = req.session.views || 0;
+// Set request handler
+app.get('/', function (req, res) {
+  var count = req.session.views || 0; // 0 it is a default value
 
-  req.session.views = ++count;
+  req.session.views = ++count; // increment value 
+                               // and save it in user session
   res.end(count + ' показ(ов)');
 });
 
